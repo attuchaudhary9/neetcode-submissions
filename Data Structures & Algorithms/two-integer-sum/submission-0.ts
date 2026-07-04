ecfb70f4c45531e2f94871701ba67ddc2d59c16c
attuchaudhary9 <68820952+attuchaudhary9@users.noreply.github.com>
2026-07-04 11:41:23 +0530

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.ts
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums: number[], target: number): number[] {
+        const seen = new Map()
+        let index = 0
+        for (const num of nums) {
+            const complement = target - num;
+            if (seen.has(complement)) {
+                return [index, seen.get(complement)]
+            }
+
+            seen.set(nums[index], index)
+            index++
+        }
+    }
+}
